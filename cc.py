c73368a2c7cf4c6725d5c88ef48c9d48cd64d8f8
“Bryan <bdscheiderer@gmail.com>
2025-07-24 20:31:02 -0500

    simple search

--- cc.py
+++ cc.py
@@ -2,28 +2,59 @@ import os
 from dotenv import load_dotenv
 from flask import Flask, flash, redirect, render_template, request, session
 from flask_session import Session
-import mysql.connector
+import sqlite3
 
+
+# set name of database
+# citycounty.db has two tables: city & county
+DATABASE = "citycounty.db"
 
 def main():
 
-    # set MySQL database name, usernmae and passord
-    username, password, database = load_env_var() 
+    # set database name, usernmae and passord
+    # username, password, database = load_env_var() 
 
+    string = input("Enter search string: ")
+    results = []
 
+    #establish database connection
+    try:
+        conn = sqlite3.connect(DATABASE)
+        c = conn.cursor()
 
-def load_env_var():
-    # load env variables and return MySQL database name, usernmae and passord
-    load_dotenv()
-    username = os.environ.get('USER')
-    password = os.environ.get('PASSWORD')
-    database = os.environ.get("DATABASE")
+        # query tables for search string
+        c.execute("SELECT * FROM county WHERE Name LIKE ?", ("%{}%".format(string),))
+        rows = c.fetchall()
 
-    if username and password and database:
-        return username, password, database
+    except Exception as e:
+        print(f"An error occurred: {e}")
+    finally:
+        if conn:
+            # conn.commit()
+            conn.close()
+
+    if len(rows) == 0:
+        print("No results found.")
+    elif len(rows) == 1:
+        print("Printing result: ")
+        print(rows)
     else:
-        print("Username, password or database name environment variables not set.")
-        # return apology
+        print(f"Printing {len(rows)} results:")
+        for row in rows:
+            print(row)
+
+# def load_env_var():
+#     # load env variables and return database name, usernmae and passord
+#     load_dotenv()
+#     username = os.environ.get('USER')
+#     password = os.environ.get('PASSWORD')
+#     database = os.environ.get("DATABASE")
+
+#     if username and password and database:
+#         return username, password, database
+#     else:
+#         print("Username, password or database name variables not set.")
+#         # return apology
 
 
 if __name__ == "__main__":
