be7d441221fa9b56063f705ee3341fd364bbdb5d
“Bryan <bdscheiderer@gmail.com>
2025-07-27 12:11:00 -0500

    working sql

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
-from flask import Flask, flash, redirect, render_template, request, session, url_for
+from flask import Flask, flash, redirect, render_template, request, session
 import sqlite3
 
 
-''' set name of database: citycounty.db has two tables--
-    city table schema: id, Name, Population, Class, County, Seat
-    county table schema: id, Name, Population, Class, Township, Seat '''
-DATABASE = "citycounty.db"
+''' set constants including name of database and table
+    citycounty schema: id, Name, Population, Class, County, Seat
+'''
+DATABASE = "citycounty.db"  # sqlite3 database located in same directory
+TABLENAME = "citycounty"    # table name in db
+MAXPOP = 9999999            # higher than state population (essentially no upper limit)
+MINPOP = 0                  # sets lower limit for search terms
 
-
+# start Flask session
 app = Flask(__name__)
 
 
@@ -54,26 +57,40 @@ def about():
 
 def get_search_results(searchData):
 
+    ''' Takes the search criteria from user input, establishes db 
+        connection, performs sql query, and returns search results
+    '''
+
     # establish database connection
     db = sqlite3.connect(DATABASE)
 
-    # use row factory for dictionary like access (instead of tuple)
+    # use row factory for dictionary-like data access (instead of a tuple)
     db.row_factory = sqlite3.Row
 
-    # create query as string
-    #query = f"Select * FROM ? WHERE name LIKE ? AND population >= ? AND population <= ?"
+    # create query as string and tuple of ? parameters
+    query = f"Select * FROM county WHERE name LIKE ? AND population >= ? AND population <= ? AND class LIKE ?"
+    query_tuple = (searchData['name'], searchData['lowerValue'], searchData['upperValue'], searchData['class'])
 
-
-    # query database for search results
+    # query database for search results, using ? placeholders
     with db:
         cursor = db.cursor()
-        cursor.execute("Select * FROM county WHERE name LIKE ? AND population >= ? AND population <= ?", (searchData['name'], searchData['lowerValue'], searchData['upperValue']))
+        cursor.execute(query, query_tuple)
         searchResults = cursor.fetchall()
+        
+        if searchResults:
+            print(searchResults[0])
+        else:
+            print("No results")
         return searchResults
 
 
 def get_search_data():
         
+        ''' This function retrieves search terms from user input 
+            entered on the /index.html search form
+        '''
+        
+        # using dictionary to store the data from the row.factory sqlite3 option
         searchData = {}
         searchData['name'] = request.form.get('name')
         searchData['lowerValue'] = request.form.get('lowerValue')
@@ -81,25 +98,38 @@ def get_search_data():
         searchData['type'] = request.form.get('type')
         searchData['class'] = request.form.get('class')
 
+        # run the clean_data function prior to return
         return clean_data(searchData)
 
 
 def clean_data(searchData):
     
-    for key in searchData:
-        if key == "name" and (searchData[key] == "" or searchData[key] == None):
+    ''' Modifies search form criteria so that the search terms 
+        match sqlite syntax and database schema
+    '''
+
+    for key, value in searchData.items():
+        # if name is blank, the "%" wildcard will allow sql to return all entries
+        if key == "name" and (value == "" or value == None):
             searchData[key] = "%"
+        # this strips any whitespace and uses "%pattern%" to match partial names
         elif key == 'name':
-             searchData[key] = f"%{searchData[key]}%"
-        elif key == "upperValue" and (searchData[key] == "" or searchData[key] == None):
-            searchData[key] = 9999999
-        elif key == "lowerValue" and (searchData[key] == "" or searchData[key] == None):
-            searchData[key] = 0    
-        elif key == "class" and (searchData['class'] == '' or searchData[key] == None):
+             searchData[key] = f"%{value.strip()}%"
+        # if max population is blank, set to a high value to return all greater than the min
+        elif key == "upperValue" and (value == "" or value == None):
+            searchData[key] = MAXPOP
+        # if min population is blank, set to zero to search for all up the max
+        elif key == "lowerValue" and (value == "" or value == None):
+            searchData[key] = MINPOP
+        # if type is blank, use "%" wildcard to search for both cities and counties
+        elif key == "type" and (value == '' or value== None):
+            searchData["class"] = "%"
+        # if class blank, uses "%" wildcard to search for any class
+        elif key == "class" and (value == '' or value== None):
             searchData["class"] = "%"
 
     return searchData
 
-
+# Use "debug=True" for development server only
 if __name__ == '__main__':
-    app.run(debug=True) # Runs the development server
+    app.run(debug=True) 
